7a4f676a8e55ed2dd2280851bcd07d05decf4ff4
Leo Zhi Kai <104268198+Leozk2000@users.noreply.github.com>
2024-01-26 10:06:29 +0800

    Update changewallpaper1.ps1

--- changewallpaper1.ps1
+++ changewallpaper1.ps1
@@ -1,5 +1,5 @@
 # Set the URL to your desired image
-$imageUrl = "https://raw.githubusercontent.com/Leozk2000/internstuff/main/hackeranyaweb.jpg"
+$imageUrl = "https://raw.githubusercontent.com/Leozk2000/internstuff/main/anikacat.jpg"
 
 # Set the local path where you want to save the downloaded image
 $downloadPath = "C:\Temp\urlimage.jpg"
