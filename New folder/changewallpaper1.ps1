8ff141e514853cc0b5bba6c5fd30ce473804248c
Leo Zhi Kai <104268198+Leozk2000@users.noreply.github.com>
2024-01-30 10:16:07 +0800

    Add files via upload

---
+++ New folder/changewallpaper1.ps1
@@ -0,0 +1,21 @@
+﻿# Set the URL to your desired image
+$imageUrl = "i.ibb.co/NpDrtk5/hackeranyaweb.jpg"
+
+# Set the local path where you want to save the downloaded image
+$downloadPath = "C:\Temp\urlimage.jpg"
+
+# Download the image from the URL
+Invoke-WebRequest -Uri $imageUrl -OutFile $downloadPath
+
+# Use the SystemParametersInfo function to change the wallpaper
+Add-Type -TypeDefinition @"
+    using System;
+    using System.Runtime.InteropServices;
+    public class Wallpaper {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+    }
+"@
+[Wallpaper]::SystemParametersInfo(20, 0, $downloadPath, 3)
+
+Write-Host "Wallpaper changed to $imageUrl"
